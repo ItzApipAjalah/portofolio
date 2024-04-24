bf64ada04d5643bdcddaa17d76d34d1010c61e61
AMWP <sayangemak011111@gmail.com>
2024-04-25 03:31:00 +0700

    add gacha waifu

--- src/js/index.js
+++ src/js/index.js
@@ -465,6 +465,66 @@ async function displayAnimeRecommendations() {
   }
 }
 
+const gachaWaifuWidget = document.getElementById('gachaWidget');
+
+gachaWaifuWidget.addEventListener('click', async function() {
+    const lastAccessTime = localStorage.getItem('waifuLastAccessTime');
+
+    if (lastAccessTime) {
+        const currentTime = new Date().getTime();
+        const elapsedTime = currentTime - parseInt(lastAccessTime);
+        
+        if (elapsedTime < 20000) { 
+            toastr.warning('Anda telah menggunakan gacha waifu dalam waktu 20 detik terakhir.');
+            return;
+        }
+    }
+
+    try {
+        const response = await fetch('https://api.waifu.pics/sfw/waifu');
+        const data = await response.json();
+        
+        const imageUrl = data.url;
+
+        Swal.fire({
+            imageUrl: '',
+            html: '<img id="waifuImage" src="' + imageUrl + '" alt="Waifu" style="filter: blur(20px); max-width: 100%; height: auto; transition: filter 0.3s;"><div id="clickText" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: white; font-size: 24px;">&#9888; Click gambar Untuk Melihat, Hati Hati Terkadang muncul NSFW &#9888;</div>',
+            showCloseButton: true,
+            showCancelButton: true,
+            cancelButtonText: 'Close',
+            confirmButtonText: 'Download',
+            didOpen: () => {
+                const waifuImage = document.getElementById('waifuImage');
+                const clickText = document.getElementById('clickText');
+
+                waifuImage.addEventListener('click', function() {
+                    waifuImage.style.filter = 'none';
+                    clickText.style.display = 'none';
+                });
+            }
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const downloadLink = document.createElement('a');
+                downloadLink.href = imageUrl;
+                downloadLink.download = 'waifu.jpg';
+                downloadLink.style.display = 'none';
+                document.body.appendChild(downloadLink);
+                downloadLink.click();
+                document.body.removeChild(downloadLink);
+            }
+        });
+
+        const currentTime = new Date().getTime();
+        localStorage.setItem('waifuLastAccessTime', currentTime.toString());
+    } catch (error) {
+        console.error('Error fetching waifu image:', error);
+        toastr.error('Terjadi kesalahan saat mengambil gambar waifu. Silakan coba lagi nanti.');
+    }
+});
+
+
+
+
 
       // Display anime recommendations when the page loads
       window.onload = displayAnimeRecommendations;
